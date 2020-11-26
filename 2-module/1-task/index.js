4c805b21da7d5a940a8a4dceedd289712f02fb0e
Vlad Korobka <vladseipa@gmail.com>
2020-11-26 17:28:49 +0200

    Module 2 is done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,4 +5,11 @@
  */
 function sumSalary(salaries) {
   // ваш код...
+  let result = 0;
+  for(let i in salaries){
+    if (typeof salaries[i] === 'number'){
+      result += salaries[i];
+    };
+  };
+  return result;
 }
