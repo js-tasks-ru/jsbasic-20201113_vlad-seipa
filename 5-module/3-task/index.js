ed195976a67e863fb3f8dde3a213bcddf01758ab
Vlad Korobka <vladseipa@gmail.com>
2020-12-08 20:43:19 +0200

    Modul 5 is done

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
   // ваш код...
+  let carouselInner = document.querySelector(".carousel__inner");
+  let slideTransformWidth = 0;
+
+  let arrowLeft = document.querySelector(".carousel__arrow_left");
+  let arrowRight = document.querySelector(".carousel__arrow_right");
+
+  arrowLeft.style.display = "none";
+
+  arrowLeft.addEventListener("click", function(){
+    slideTransformWidth -= carouselInner.offsetWidth;
+    carouselInner.style.transform = "translateX(" + slideTransformWidth*-1 + "px)";
+
+    if (slideTransformWidth === 0){
+      arrowLeft.style.display = "none";
+    };
+
+    if (slideTransformWidth !== carouselInner.offsetWidth*4){
+      arrowRight.style.display = "";
+    };
+  });
+
+  arrowRight.addEventListener("click", function(){
+    slideTransformWidth += carouselInner.offsetWidth;
+    carouselInner.style.transform = "translateX(-" + slideTransformWidth + "px)";
+
+    if (slideTransformWidth > carouselInner.offsetWidth*2){
+      arrowRight.style.display = "none";
+    };
+
+    if (slideTransformWidth > 0){
+      arrowLeft.style.display = "";
+    };
+  });
 }
