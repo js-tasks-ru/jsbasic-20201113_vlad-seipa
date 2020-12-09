f12b53fe8c56891167d42864dbf34cfc66287d56
Vlad Korobka <vladseipa@gmail.com>
2020-12-09 21:46:48 +0200

    Module 5, update Task 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -3,10 +3,45 @@ function initCarousel() {
   let carouselInner = document.querySelector(".carousel__inner");
   let slideTransformWidth = 0;
 
+  let carousel = document.querySelector(".carousel");
+
+  document.querySelector(".carousel__arrow_left").style.display = "none";
+
+  carousel.addEventListener("click", function(event){
+    if (event.target.closest("div.carousel__arrow_right")){
+      slideTransformWidth += carouselInner.offsetWidth;
+      carouselInner.style.transform = "translateX(-" + slideTransformWidth + "px)";
+  
+      if (slideTransformWidth > carouselInner.offsetWidth*2){
+        document.querySelector(".carousel__arrow_right").style.display = "none";
+      };
+  
+      if (slideTransformWidth > 0){
+        document.querySelector(".carousel__arrow_left").style.display = "";
+      };
+
+    } else if(event.target.closest("div.carousel__arrow_left")){
+      slideTransformWidth -= carouselInner.offsetWidth;
+      carouselInner.style.transform = "translateX(" + slideTransformWidth*-1 + "px)";
+  
+      if (slideTransformWidth === 0){
+        document.querySelector(".carousel__arrow_left").style.display = "none";
+      };
+  
+      if (slideTransformWidth !== carouselInner.offsetWidth*4){
+        document.querySelector(".carousel__arrow_right").style.display = "";
+      };
+
+    } else {
+      return;
+    };
+  });
+
+/*
+  // ПРЕЖНЯЯ ВЕРСИЯ
+
   let arrowLeft = document.querySelector(".carousel__arrow_left");
   let arrowRight = document.querySelector(".carousel__arrow_right");
-
-  arrowLeft.style.display = "none";
 
   arrowLeft.addEventListener("click", function(){
     slideTransformWidth -= carouselInner.offsetWidth;
@@ -33,4 +68,5 @@ function initCarousel() {
       arrowLeft.style.display = "";
     };
   });
+*/
 }
